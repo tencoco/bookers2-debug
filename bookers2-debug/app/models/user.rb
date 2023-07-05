46b1f06bd5c20670fef05f0d01f2c03b5766f6d3
tencoco <135337078+tencoco@users.noreply.github.com>
2023-07-05 13:23:31 +0000

    [Finish]bookers2-debug-3

--- bookers2-debug/app/models/user.rb
+++ bookers2-debug/app/models/user.rb
@@ -7,6 +7,13 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+
+  has_many :following_users, class_name:"Relationship", foreign_key:"follower_id", dependent: :destroy
+  has_many :follower_users, class_name:"Relationship", foreign_key:"followed_id", dependent: :destroy
+
+  has_many :follows, through: :following_users, source: :followed
+  has_many :followers, through: :follower_users, source: :follower
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -15,5 +22,18 @@ class User < ApplicationRecord
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
+  # フォローしたときの処理
+  def follow(user_id)
+    following_users.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    following_users.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    follows.include?(user)
   end
 end
--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "home/about"=>"homes#about"
   devise_for :users
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get "follows" => "relationships#follows"
+    get "followers" => "relationships#followers"
+  end
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
