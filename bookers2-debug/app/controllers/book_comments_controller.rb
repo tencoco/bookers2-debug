839fafa97c8e688eda1800bf8c1aa307d2dd3ea6
tencoco <135337078+tencoco@users.noreply.github.com>
2023-07-10 14:26:58 +0000

    [Finish]bookers2-debug-theme6

--- bookers2-debug/app/controllers/book_comments_controller.rb
+++ bookers2-debug/app/controllers/book_comments_controller.rb
@@ -1,16 +1,17 @@
 class BookCommentsController < ApplicationController
   before_action :is_matching_login_user, only: [:destroy]
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
     redirect_to request.referer
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    #redirect_to request.referer
   end
 
   private
